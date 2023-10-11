bbacf2914437ab80acf2dda675e3291c20a597cc
Reza <mrneyestani@gmail.com>
2023-10-11 21:16:20 +0200

    add env file to gitignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const { User } = require("./db/mongo");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const PORT = 4000;
 
@@ -19,44 +21,65 @@ app.post("/api/auth/login", login);
 app.listen(PORT, function () {
   console.log(`Server is runnig on: ${PORT}`);
 });
-const users = [];
 
-function signUp(req, res) {
+async function signUp(req, res) {
   const body = req.body;
   const email = req.body.email;
   const password = req.body.password;
 
-  const userInDb = users.find((user) => user.email === body.email);
+  const userInDb = await User.findOne({
+    email: email,
+  });
+
   if (userInDb != null) {
     res.status(400).send("Email already exists");
     return;
   }
+
+  function hashPassword(password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    return hash;
+  }
+
   const user = {
     email: email,
-    password: password,
+    password: hashPassword(password),
   };
-  users.push(user);
+
+  try {
+    await User.create(user);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Something went wrong");
+    return;
+  }
   res.send("Sign up");
 }
-function login(req, res) {
+async function login(req, res) {
   const body = req.body;
 
-  console.log("body:", body);
-  console.log("user in db:", users);
+  const userInDb = await User.findOne({
+    email: body.email,
+  });
 
-  const userInDb = users.find((user) => user.email === body.email);
   if (userInDb == null) {
     res.status(401).send("Wrong email");
     return;
   }
 
   const passwordInDb = userInDb.password;
-  if (passwordInDb != body.password) {
+  if (!isPasswordCorrect(req.body.password, passwordInDb)) {
     res.status(401).send("Wrong password");
     return;
   }
   res.send({
-    userId: "123",
+    userId: userInDb._id,
     token: "token",
   });
+
+  function isPasswordCorrect(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  }
 }
